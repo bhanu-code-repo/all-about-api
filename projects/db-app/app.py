749abbc892d7ac6a859cf2ef24bae575bc635800
Bhanu Pratap Singh Thakur <bpst.work@gmail.com>
2022-04-02 20:23:05 +0530

    db app

---
+++ projects/db-app/app.py
@@ -0,0 +1,144 @@
+# importing required packages
+import os, utils
+from flask import Flask, request, jsonify
+import mysql.connector as conn
+
+# create app - object of Flask class
+app = Flask(__name__)
+
+@app.route('/configure-db', methods=['POST'])
+def configure_db():
+    if utils.check_content_type(request):
+        request_body = {k.lower(): v.lower() for (k, v) in request.json.items()}
+        if utils.validate_request_body(['username', 'password'], request_body.keys()):
+            utils.write_json_file('config.json', request_body)
+            return jsonify({'status': 'db configured successfully'})
+        else:
+            return jsonify({'error': 'username or password is missing'})
+    return jsonify({'error': 'Content-Type not supported!'})
+
+
+@app.route('/get-dbs', methods=['GET'])
+def get_db():
+    if 'config.json' in os.listdir('.'):
+        app_config = utils.read_json_file('config.json')
+        try:
+            # create cursor
+            my_db = conn.connect(host='localhost', user=str(app_config['username']), passwd=str(app_config['password']))
+            cursor = my_db.cursor()
+
+            # process request
+            status, result = utils.process_get_dbs_request(request, cursor)
+            if status:
+                return jsonify({'existing dbs': result})
+            else:
+                return jsonify({'error': result})
+        except conn.Error as err:
+            return jsonify({'message': err.msg, 'error code': err.errno, 'sqlstate': err.sqlstate})
+    else:
+        return jsonify({'error': 'db configuration file not exists'})
+
+
+@app.route('/check-db', methods=['GET'])
+def check_db():
+    if 'config.json' in os.listdir('.'):
+        app_config = utils.read_json_file('config.json')
+        try:
+            # create cursor
+            my_db = conn.connect(host='localhost', user=str(app_config['username']), passwd=str(app_config['password']))
+            cursor = my_db.cursor()
+
+            # process request
+            status, result = utils.process_get_db_request(request, cursor)
+            if status:
+                db_status = 'exists' if int(result.split('#')[1]) == 1 else 'not exists'
+                return jsonify({'db name': result.split('#')[0], 'db status': db_status})
+            else:
+                return jsonify({'error': result})
+        except conn.Error as err:
+            return jsonify({'message': err.msg, 'error code': err.errno, 'sqlstate': err.sqlstate})
+    else:
+        return jsonify({'error': 'db configuration file not exists'})
+
+
+@app.route('/create-db', methods=['GET', 'POST'])
+def create_db():
+    # handle all GET requests
+    if request.method == 'GET':
+        return jsonify({'error': 'check request type, it should be POST'})
+
+    # handle all POST requests
+    if request.method == 'POST':
+        if 'config.json' in os.listdir('.'):
+            app_config = utils.read_json_file('config.json')
+            try:
+                # create cursor
+                my_db = conn.connect(host='localhost', user=str(app_config['username']),
+                                     passwd=str(app_config['password']))
+                cursor = my_db.cursor()
+
+                # process request
+                status, result = utils.process_create_db_request(request, cursor)
+                if status:
+                    jsonify({'db status': result})
+                else:
+                    return jsonify({'error': result})
+            except conn.Error as err:
+                return jsonify({'message': err.msg, 'error code': err.errno, 'sqlstate': err.sqlstate})
+        else:
+            return jsonify({'error': 'db configuration file not exists'})
+
+
+@app.route('/get-db-data', methods=['GET', 'POST'])
+def get_db_data():
+    # handle all GET requests
+    if request.method == 'GET':
+        return jsonify('supported database: SQL, MongoDB')
+
+    # handle all POST requests
+    if request.method == 'POST':
+        if 'config.json' in os.listdir('.'):
+            app_config = utils.read_json_file('config.json')
+            try:
+                # create cursor
+                my_db = conn.connect(host='localhost', user=str(app_config['username']),
+                                     passwd=str(app_config['password']))
+                cursor = my_db.cursor()
+
+                # process request
+                status, result = utils.process_get_db_data(request, cursor)
+                if status:
+                    return jsonify({'data': str(result)})
+                else:
+                    return jsonify({'error': result})
+            except conn.Error as err:
+                return jsonify({'message': err.msg, 'error code': err.errno, 'sqlstate': err.sqlstate})
+        else:
+            return jsonify({'error': 'db configuration file not exists'})
+
+
+@app.route('/insert-data', methods=['POST'])
+def insert_data():
+    if 'config.json' in os.listdir('.'):
+        app_config = utils.read_json_file('config.json')
+        try:
+            # create cursor
+            my_db = conn.connect(host='localhost', user=str(app_config['username']),
+                                 passwd=str(app_config['password']))
+            cursor = my_db.cursor()
+
+            request_body = {k.lower(): v.lower() for (k, v) in request.json.items()}
+            if utils.validate_request_body(['name', 'type', 'query'], request_body.keys()):
+                if request_body['type'] == 'sql':
+                    cursor.execute(request_body['query'])
+                    my_db.commit()
+                    return jsonify({'status': 'record added successfully'})
+        except conn.Error as err:
+            return jsonify({'message': err.msg, 'error code': err.errno, 'sqlstate': err.sqlstate})
+    else:
+        return jsonify({'error': 'db configuration file not exists'})
+
+
+# run flask app - by default flask will run on port number 5000
+if __name__ == '__main__':
+    app.run(host='localhost', port=9000, debug=True)
---
+++ projects/db-app/utils.py
@@ -0,0 +1,126 @@
+# importing required packages
+import os, json, enum
+import mysql.connector as conn
+
+
+# class ConfigFile(enum.Enum):
+#     FileExits = 1
+#     FileNotFound = 2
+
+def check_content_type(request):
+    status = False
+    # get request "Content-Type"
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json':
+        status = True
+    return status
+
+
+def read_json_file(filename):
+    with open(filename, 'r') as openfile:
+        json_object = json.load(openfile)
+    return json_object
+
+
+def write_json_file(filename, data):
+    json_object = json.dumps(data, indent=2)
+    with open(filename, 'w') as outfile:
+        outfile.write(json_object)
+
+
+# def get_sql_db_connection():
+#     if 'config.json' in os.listdir('.'):
+#         print(read_json_file('config.json'))
+#         app_config = read_json_file('config.json')
+#         return conn.connect(host='localhost', user=app_config['username'], passwd=app_config['password'])
+#     else:
+#         return ConfigFile.FileNotFound
+
+
+def validate_request_body(required_keys, actual_keys):
+    status = True
+    for key in required_keys:
+        if key not in actual_keys:
+            status = False
+    return status
+
+
+def process_get_dbs_request(request, cursor):
+    status = False
+    args_dict = {k.lower(): v.lower() for (k, v) in request.args.to_dict().items()}
+    if len(args_dict.items()) == 1:
+        if 'type' in args_dict.keys():
+            if args_dict['type'] == 'sql':
+                status = True
+                cursor.execute('show databases')
+                return status, str([db_name for db_name, in cursor.fetchall()])
+            elif args_dict['type'] == 'mongodb':
+                status = True
+                return status, 'to do'
+            else:
+                return status, 'invalid db type'
+        else:
+            return status, 'db type not specified in url'
+    else:
+        return status, 'invalid request'
+
+
+def process_get_db_request(request, cursor):
+    status = False
+    args_dict = {k.lower(): v.lower() for (k, v) in request.args.to_dict().items()}
+    if len(args_dict.items()) == 2:
+        if 'db' in args_dict.keys() and 'type' in args_dict.keys():
+            if args_dict['type'] == 'sql':
+                status = True
+                cursor.execute('show databases')
+                result = 1 if args_dict["db"] in [str(db_name) for db_name, in cursor.fetchall()] else 0
+                return status, f'{args_dict["db"]}#{result}'
+            elif args_dict['type'] == 'mongodb':
+                status = True
+                return status, 'to do'
+            else:
+                return status, 'invalid db type'
+        else:
+            return status, 'either db or db type or both not specified in url'
+    else:
+        return status, 'invalid request'
+
+
+def process_create_db_request(request, cursor):
+    status = False
+    if check_content_type(request):
+        request_body = {k.lower(): v.lower() for (k, v) in request.json.items()}
+        if validate_request_body(['name', 'type'], request_body.keys()):
+            if request_body['type'] == 'sql':
+                status = True
+                cursor.execute(f'CREATE DATABASE IF NOT EXISTS {request_body["name"]}')
+                return status, f'{request_body["name"]} db created successfully'
+            elif request_body['type'] == 'mongodb':
+                status = True
+                return status, 'to do'
+            else:
+                return status, 'invalid db type'
+        else:
+            return status, 'request body validation'
+    else:
+        return status, 'Content-Type not supported!'
+
+
+def process_get_db_data(request, cursor):
+    status = False
+    if check_content_type(request):
+        request_body = {k.lower(): v.lower() for (k, v) in request.json.items()}
+        if validate_request_body(['name', 'type', 'query'], request_body.keys()):
+            if request_body['type'] == 'sql':
+                status = True
+                cursor.execute(request_body['query'])
+                return status, cursor.fetchall()
+            elif request_body['type'] == 'mongodb':
+                status = True
+                return status, 'to do'
+            else:
+                return status, 'invalid db type'
+        else:
+            return status, 'request body validation'
+    else:
+        return status, 'Content-Type not supported!'
