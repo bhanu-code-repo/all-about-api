55ada9673e468672fa28f5a28286ce784dcbdc0b
Bhanu Pratap Singh Thakur <bpst.work@gmail.com>
2022-04-02 11:52:08 +0530

    simple flask app

--- projects/simple-flask-api/app.py
+++ projects/simple-flask-api/app.py
@@ -1,3 +1,16 @@
 # importing required packages
-import flask
+from flask import Flask, request, jsonify
 
+# create app - object of Flask class
+# note: "app" name can be anything you like, it's just object name i.e. bhanu = Flask(__name__)
+app = Flask(__name__)
+
+
+# create home route with GET method
+@app.route('/', methods=['GET'])
+def home():
+    return 'hello, welcome to flask app'
+
+
+# run flask app
+app.run(host='localhost', port=9000, debug=True)
